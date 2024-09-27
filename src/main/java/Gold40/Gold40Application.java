40e6d28aa6a36df94a455454a85b0fb51fc78c20
Minh1009 <154089717+minhtinhabc0@users.noreply.github.com>
2024-09-28 00:50:18 +0700

    create

---
+++ src/main/java/Gold40/Gold40Application.java
@@ -0,0 +1,13 @@
+package Gold40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gold40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gold40Application.class, args);
+	}
+
+}
---
+++ src/test/java/Gold40/Gold40ApplicationTests.java
@@ -0,0 +1,13 @@
+package Gold40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gold40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
