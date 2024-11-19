85a0bf6ba1cd2114f0388bce5d56eb84adc31126
Trung Bui <builun1811@gmail.com>
2024-11-19 20:19:59 +0700

    baocaonpp

---
+++ src/main/java/Gold40/DAO/BaoCaoNPPDAO.java
@@ -0,0 +1,7 @@
+package Gold40.DAO;
+
+import Gold40.Entity.BaoCaoNPP;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BaoCaoNPPDAO extends JpaRepository<BaoCaoNPP, Integer> {
+}
